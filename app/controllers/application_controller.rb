43861021d4e87e4c82daaa18ea0f15337fd08356
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-18 03:12:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,6 @@ class ApplicationController < Sinatra::Base
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
-    # session[:id] = @user.id
-    # redirect '/users/home'
   end
 
   get '/sessions/login' do
